592c1daf2a175d15e96467569444964c182835f1
Rohan Phuyal <rohanphuyal2@gmail.com>
2024-11-22 10:23:51 +0545

    Added score and jackpot spins

--- assets/scripts/WheelSpinner.ts
+++ assets/scripts/WheelSpinner.ts
@@ -23,12 +23,22 @@ export default class WheelSpinner extends cc.Component {
     exitButton: cc.Node = null;
     @property(cc.Node)
     freeSpin: cc.Node = null;
+    @property(cc.Node)
+    noCashPopup: cc.Node = null;
+    @property(cc.Node)
+    winPopup: cc.Node = null;
     @property totalSections = 8;
     @property offset = -22.5;
     @property desired = 0;
     @property({ type: cc.Integer, range: [0, 5, 1] })
-    stopTime: number = 1;
-    score = 0;
+    minSpinTimeWheel: number = 1;
+    @property({ type: cc.Integer, range: [0, 5, 1] })
+    maxSpinTimeWheel: number = 5;
+    @property({ type: cc.Integer })
+    minSpinAngle: number = 360;
+    @property({ type: cc.Integer })
+    maxSpinAngle: number = 3600;
+    @property cash = 10;
     private lastMilestone: number = 0; // Tracks the last milestone score
 
     private currentGameState: GameState = GameState.Start; // Initial game state
@@ -38,6 +48,11 @@ export default class WheelSpinner extends cc.Component {
 
     @property([cc.String]) wheelValues: string[] = ["1", "2", "3", "4", "5", "6", "7", "Jackpot"];
 
+    @property({ type: cc.Integer })
+    minJackpotAmount: number = 2;
+    @property({ type: cc.Integer })
+    maxJackpotAmount: number = 7;
+
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
@@ -45,6 +60,7 @@ export default class WheelSpinner extends cc.Component {
     }
 
     start() {
+        this.scoreNode.getComponent(cc.Label).string = "$ " + this.cash;
         this.updateButtonsState();
     }
 
@@ -72,6 +88,10 @@ export default class WheelSpinner extends cc.Component {
         }
     }
 
+    getRandomValueBetween(minValue: number, maxValue: number): number {
+        return Math.random() * (maxValue - minValue) + minValue;
+    }
+
     onSpinButtonClick() {
         cc.log("clicked spin button");
         //gaurd clause
@@ -79,7 +99,13 @@ export default class WheelSpinner extends cc.Component {
             return;
 
         if (this.currentGameState === GameState.Start || this.currentGameState === GameState.End) {
-            this.startAnimation();
+            if (this.cash <= 0) {
+                this.noCashAvailablePopup();
+            } else {
+                this.cash--;
+                this.scoreNode.getComponent(cc.Label).string = "$ " + this.cash;
+                this.startAnimation();
+            }
         }
 
     }
@@ -87,11 +113,12 @@ export default class WheelSpinner extends cc.Component {
         this.currentGameState = GameState.Spinning; // Set game state to Spinning
         this.updateButtonsState(); // Disable the play/exit button
         // let randomNumber=Math.floor(Math.random()*8+1);
-        let randomNumber = Math.random();
+        let randomSpinAngle = this.getRandomValueBetween(this.minSpinAngle, this.maxSpinAngle);
         // cc.log("Random Number: " + randomNumber);
-        let randDuration = Math.floor(Math.random() * 5 + this.stopTime);
+        let randomDuration = this.getRandomValueBetween(this.minSpinTimeWheel, this.maxSpinTimeWheel);
+        cc.log("Random Angle: " + randomSpinAngle + " \nRandom Duration: " + randomDuration)
         cc.tween(this.wheelNode)
-            .by(randDuration, { angle: 360 * randomNumber * 5 }, { easing: "quartOut" })
+            .by(randomDuration, { angle: randomSpinAngle }, { easing: "quartOut" })
             .call(() => this.calculateResult())
             .start();
     }
@@ -124,9 +151,7 @@ export default class WheelSpinner extends cc.Component {
             index = 0;
         }
 
-        this.currentGameState = GameState.End; // Set game state to End
-        this.updateButtonsState(); // Re-enable the play/exit button
-        this.countScore(index);
+        this.youWinPopup(index);
 
 
         // Display the result
@@ -134,30 +159,62 @@ export default class WheelSpinner extends cc.Component {
         // cc.log("Adjusted Angle: " + adjustedAngle);
         // cc.log("The wheel stopped at index: " + index);
     }
-    private countScore(index: number) {
-        let scoreIndex = index;
-        if (this.scoreNode.getComponent(cc.Label).string === "$0") {
-            if (this.wheelNode.getComponentsInChildren(cc.Label)[scoreIndex].string == "Jackpot") {
-                this.freeSpinWin();
-            } else {
-                this.score = parseInt(this.wheelNode.getComponentsInChildren(cc.Label)[scoreIndex].string);
-            }
-        } else {
-            if (this.wheelNode.getComponentsInChildren(cc.Label)[scoreIndex].string == "Jackpot") {
-                this.freeSpinWin();
-            } else {
-                let tempScore = parseInt(this.wheelNode.getComponentsInChildren(cc.Label)[scoreIndex].string);
-                this.score += tempScore;
-            }
-        }
-        this.scoreNode.getComponent(cc.Label).string = "$ " + this.score;
 
+    private youWinPopup(index: number) {
+        const labelValue = this.wheelNode.getComponentsInChildren(cc.Label)[index].string;
+
+        if (labelValue === "Jackpot") {
+            let jackpotAmount = Math.floor(this.getRandomValueBetween(this.minJackpotAmount, this.maxJackpotAmount));
+            this.showWinPopup("+$" + jackpotAmount); // Show +2 for Jackpot
+            cc.tween(this.winPopup)
+                .by(1, { scale: 1 }, { easing: "quartOut" })
+                .call(() => {
+                    this.winPopup.active = false;
+                    this.addCash(jackpotAmount);
+                    this.freeSpinWin();
+                })
+                .start();
+        } else {
+            this.showWinPopup("+$" + labelValue);
+            cc.tween(this.winPopup)
+                .by(1, { scale: 1 }, { easing: "quartOut" })
+                .call(() => this.countCash(index))
+                .start();
+        }
     }
+
+    private showWinPopup(value: string) {
+        this.winPopup.getComponent(cc.Label).string = value;
+        this.winPopup.active = true;
+        this.winPopup.scale = 1; // Reset scale to default
+    }
+
+    private addCash(amount: number) {
+        this.cash += amount;
+        this.scoreNode.getComponent(cc.Label).string = "$ " + this.cash;
+    }
+
+    private countCash(index: number) {
+        const labelValue = this.wheelNode.getComponentsInChildren(cc.Label)[index].string;
+
+        this.winPopup.active = false;
+        const winAmount = parseInt(labelValue, 10) || 0;
+        this.addCash(winAmount);
+
+        this.currentGameState = GameState.End; // Set game state to End
+        this.updateButtonsState(); // Re-enable the play/exit button
+    }
+
 
     private updateButtonsState() {
         cc.log("GameState: " + this.currentGameState);
         if (this.playButton) {
             if (this.currentGameState === GameState.Start || this.currentGameState === GameState.End) {
+                // if (this.cash <= 0) {
+                //     this.playButton.getComponent(cc.Button).interactable = false;
+                // } else {
+                //     this.playButton.getComponent(cc.Button).interactable = true;
+                // }
                 this.playButton.getComponent(cc.Button).interactable = true;
                 this.exitButton.getComponent(cc.Button).interactable = true;
             } else {
@@ -184,15 +241,26 @@ export default class WheelSpinner extends cc.Component {
         this.startAnimation();
     }
 
-    onExitButtonClick(){
+    onExitButtonClick() {
         this.currentGameState = GameState.Start; // Set game state to Start
         this.updateButtonsState(); // Disable the play/exit button
         // Find the SceneManager node
         const sceneManagerNode = cc.find("Canvas/SceneManager");
         this.sceneManagerController = sceneManagerNode.getComponent("SceneManagerController");
         this.sceneManagerController.onGameExit();
-        cc.log("sceneManagerNode "+sceneManagerNode);
+        cc.log("sceneManagerNode " + sceneManagerNode);
     }
 
+    private noCashAvailablePopup() {
+        this.noCashPopup.active = true;
+        this.noCashPopup.scale = 1; // Reset scale to default
+        cc.tween(this.noCashPopup)
+            .by(1, { scale: 1 }, { easing: "quartOut" })
+            .call(() => this.finishNoCashAnimation())
+            .start();
+    }
+    private finishNoCashAnimation() {
+        this.noCashPopup.active = false;
+    }
     // update (dt) {}
 }
